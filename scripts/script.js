a4455b7f43d69ced1085e71eb56203ae445eb920
Michail Kozlov <red.bymike@gmail.com>
2023-02-27 12:25:07 +0300

    created calculator

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const select = document.querySelector(".select_hose");
+const flow_rate = document.querySelector(".flow_rate");
+const hose_length = document.querySelector(".hose_length");
+const total_friction_loss = document.querySelector(".total_friction_loss");
+const friction_loss = document.querySelector(".friction_loss");
+
+const calculate = () => {
+  const total_friction_loss_value =
+    Number(select.value) *
+    Math.pow(flow_rate.value / 100, 2) *
+    (hose_length.value / 100);
+
+  const friction_loss_value =
+    (total_friction_loss_value / hose_length.value) * 50;
+
+  total_friction_loss.innerHTML =
+    Math.round(total_friction_loss_value * 10) / 10;
+  friction_loss.innerHTML = isNaN(friction_loss_value)
+    ? 0
+    : Math.round(friction_loss_value * 10) / 10;
+};
+
+select.addEventListener("change", calculate);
+flow_rate.addEventListener("change", calculate);
+hose_length.addEventListener("change", calculate);
